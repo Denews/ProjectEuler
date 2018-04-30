b3571f5fa0fc22dadbb71ed5e66510295fb50c5e
Denews <gkvms963@naver.com>
2018-04-30 15:30:55 +0900

    5번 다시품
    풀이보고 다시품. 겁나 신기하네;;

--- RustEuler/src/p005.rs
+++ RustEuler/src/p005.rs
@@ -1,9 +1,12 @@
-use num::integer::Integer;
+use num::pow;
 
 pub fn solve() -> i32 {
+    let plist = [2, 3, 5, 7, 11, 13, 17, 19];
     let mut result:i32 = 1;
-    for i in 1..20 {
-        result = result * i / result.gcd(&i);
+
+    for i in plist.iter() {
+        result *= pow(*i, (20 as f64).log(*i as f64).floor() as usize);
     }
+
     result
 }
